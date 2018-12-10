5906daec0769e6ea34d8563858cf4cc36eb97f28
rebeccaarce <rebecca1@bu.edu>
2018-12-09 20:26:37 -0500

    Add files via upload

--- routes/generate_rebecca.js
+++ routes/generate_rebecca.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var request = require('request');
 
 /* Generate button functionality. */
 router.post('/', function(req, res, next) {
-    console.log(req.body);
 
     var input_playlist = [];
     var selectCount = Object.keys(req.body).length;
@@ -16,7 +16,7 @@ router.post('/', function(req, res, next) {
 
     console.log(input_playlist);
 
-
+    
     //mysql code
     var connection = mysql.createConnection( {
         host: 'localhost',
@@ -241,7 +241,7 @@ router.post('/', function(req, res, next) {
                             all_dur[i] = Number(result[i].duration);
                             total_high_duration = total_high_duration + all_dur[i];
                         }
-
+                        
                         if ((total_high_duration > 0) && (total_high_duration <= input_duration))
                         {
                             // add all songs from select query into list
@@ -263,6 +263,9 @@ router.post('/', function(req, res, next) {
                                 }
                             }
                         }
+
+
+                         
                         console.log(final_playlist);
 
                     }
@@ -270,9 +273,132 @@ router.post('/', function(req, res, next) {
 
         }
     }
+    
 
-    // last step
-    res.json( {playlistSongs: sliderSelections});
+    // artificial final_playlist for testing
+    //var final_playlist = ['spotify:track:2nMeu6UenVvwUktBCpLMK9', 'spotify:track:0mt02gJ425Xjm7c3jYkOBn', 'spotify:track:487OPlneJNni3NWC8SYqhW'];
+
+    console.log('Starting calls');
+
+    for (var i=0; i<final_playlist.length; i++) 
+    {
+        var append = 'spotify::track::';
+        var playlist = final_playlist[i].toString();
+        final_playlist[i] = append+playlist;
+    }
+
+    var final_playlist_JSON_form = {'uris' : final_playlist};
+    var final_playlist_query_form = 'uris=';
+
+    for (var i = 0; i < final_playlist.length; i++) {
+        final_playlist_query_form += final_playlist[i] + ',';
+    }
+
+    // get rid of trailing comma
+    final_playlist_query_form = final_playlist_query_form.slice(0, -1);
+
+    console.log('Playlist in JSON form done');
+    console.log(final_playlist_JSON_form);
+
+    var playlistOptions = { method: 'GET',
+        url: 'https://api.spotify.com/v1/me/playlists',
+        headers:
+            { 'Postman-Token': '43903693-d2df-4544-a1c6-5527210828f3',
+                'cache-control': 'no-cache',
+                Authorization: 'Bearer ' + req.session.access_token },
+        json: true};
+
+    request(playlistOptions, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log('Successfully got playlists of user');
+
+        var playlistArray = body.items;
+        var flag = false;
+        var targetIndex = 0;
+
+        // check if user already has the TuneActive Playlist
+        for (var i = 0; i < playlistArray.length; i++) {
+            if (playlistArray[i].name === 'TuneActive Playlist') {
+                flag = true;
+                targetIndex = i;
+                break;
+            }
+        }
+
+        if (flag) {
+            var targetID = playlistArray[targetIndex].id;
+            console.log('Playlist already exists with ID: ' + targetID);
+
+            var replaceOptions = { method: 'PUT',
+                url: 'https://api.spotify.com/v1/playlists/' + targetID + '/tracks',
+                //qs: final_playlist_JSON_form,
+                headers:
+                    { 'Postman-Token': '5ccf2a8f-d6c9-487d-b0de-96aeef897a73',
+                        'cache-control': 'no-cache',
+                        'Content-Type': 'application/json',
+                        Authorization: 'Bearer ' + req.session.access_token},
+                body: final_playlist_JSON_form,
+                json: true};
+
+            request(replaceOptions, function (error, response, body) {
+                if (error) throw new Error(error);
+                console.log('Successfully replaced songs in playlist');
+
+                // everything added, send back the playlist ID to be shown in an embedded spotify element
+                res.json({playlistID: targetID});
+
+            });
+
+        } else {
+            // playlist doesn't exist yet, create it
+            console.log("Playlist doesn't exist.");
+
+            var newPlayOptions = { method: 'POST',
+                url: "https://api.spotify.com/v1/users/"+req.session.userId+"/playlists",
+                body: JSON.stringify({
+                    'name': "TuneActive Playlist",
+                    'public': false
+                }),
+                dataType: 'json',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + req.session.access_token
+                }
+            };
+
+
+            request(newPlayOptions, function (error,response,body) {
+                console.log('Successfully created new playlist');
+                var newBody = JSON.parse(body);
+                var playlistID = newBody.id;
+
+                var addOptions = { method: 'POST',
+                    url: 'https://api.spotify.com/v1/playlists/' + playlistID + '/tracks',
+                    //qs: final_playlist_JSON_form,
+                    headers:
+                        { 'Postman-Token': '5ccf2a8f-d6c9-487d-b0de-96aeef897a73',
+                            'cache-control': 'no-cache',
+                            'Content-Type': 'application/json',
+                            Authorization: 'Bearer ' + req.session.access_token},
+                    body: final_playlist_JSON_form,
+                    json: true};
+
+                request(addOptions, function (error, response, body) {
+                    if (error) throw new Error(error);
+                    console.log('Successfully added songs to playlist');
+                    console.log(body);
+
+                    // everything added, send back the playlist ID to be shown in an embedded spotify element
+                    res.json({playlistID: targetID});
+
+                });
+
+            });
+        }
+
+    });
+
 });
 
 module.exports = router;
